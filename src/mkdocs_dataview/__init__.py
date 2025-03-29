3a8e9b0beb849a4be65cf8813d409c632647d1d8
Alexander Nevskiy <kepkin@gmail.com>
2025-03-29 23:22:41 +0100

    fixes

--- src/mkdocs_dataview/__init__.py
+++ src/mkdocs_dataview/__init__.py
@@ -1,1 +1,5 @@
-version = "0.1.0"
+"""
+__init__ module for dataview package
+"""
+
+__version__ = "0.1.0"
--- src/mkdocs_dataview/utils.py
+++ src/mkdocs_dataview/utils.py
@@ -3,18 +3,14 @@ Helper module to work with files.
 """
 import os.path
 
+
 def enumerate_files_by_ext(path, ext_list=None):
     """
     Generator for files filtered by extension
     """
     for root, dirs, files in os.walk(path):
-        keys = []
-        if root != path:
-            directory = os.path.relpath(root, path)
-            keys = directory.split(os.sep)
-
         for file in files:
-            if ext_list != None:
+            if ext_list is not None:
                 _, extension = os.path.splitext(file)
                 if extension in ext_list:
                     yield os.path.join(root, file)
